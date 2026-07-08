0ac229ec944af78be30520e41938d90dcf6084fc
Tejaswini466 <nagatejaswini.p@gmail.com>
2026-07-08 15:33:25 +0530

    File updates

--- TwoWayPointers/trappingRainwater.py
+++ TwoWayPointers/trappingRainwater.py
@@ -1,5 +1,5 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    def trap(self, height) -> int:
         left, right = 0, len(height) - 1
         left_max, right_max = 0, 0
         water = 0
